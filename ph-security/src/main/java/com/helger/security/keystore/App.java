afec64832b4751b9c8ae9a94b274a422b5a8b606
Michiel de Jong <michiel@unhosted.org>
2021-07-13 16:17:24 +0200

    Debug https://github.com/pondersource/peppol-testsuite/issues/1#issuecomment-879128138

---
+++ ph-security/src/main/java/com/helger/security/keystore/App.java
@@ -0,0 +1,17 @@
+import com.helger.security.keystore.LoadedKeyStore;
+import com.helger.security.keystore.KeyStoreHelper;
+import com.helger.security.keystore.EKeyStoreType;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        final LoadedKeyStore ks = KeyStoreHelper.loadKeyStore(
+            EKeyStoreType.PKCS12, 
+            "/Users/michiel/Downloads/pondersource-c2.pfx",
+            "123456");
+        // final LoadedKey k = KeyStore.loadPrivateKey()
+        System.out.println( "Hello World!" );
+    }
+}
+
